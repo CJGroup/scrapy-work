33595256c4aad4c4c5039557353610c3861deaea
ConstasJ <constasj@qq.com>
2024-01-31 18:46:52 +0800

    feat: 完成主体功能

---
+++ src/scrapy-work/main.py
@@ -0,0 +1,22 @@
+import json
+from requests import get
+from bs4 import BeautifulSoup
+
+
+def dump_to_file(obje):
+    json.dump(obje, open("bs.json", "w"), ensure_ascii=False, indent=4)
+
+
+def main():
+    url = "https://www.zdic.net/zd/bs/"
+    html = get(url).text
+    soup = BeautifulSoup(html, "lxml")
+    tables = soup.find("div", class_="nr-box").find_all("table")
+    bs_dict = {}
+    for table in tables:
+        bsyx = table.find(class_="bsyx").text
+        bsul = []
+        for pck in table.find_all(class_="pck"):
+            bsul.append(pck.text)
+        bs_dict[bsyx] = bsul
+    dump_to_file(bs_dict)
